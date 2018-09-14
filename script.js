9f3460211ba2d9979187fe4f475706e29380a972
MoringaSchool <kamadidouglas@gmail.com>
2018-09-14 17:09:02 +0300

    add the background image

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-var triangle = function(sideA, sideB, sideC){
-    if (isTriangle(sideA, sideB, sideC)) {
-      return triangleType(sideA, sideB, sideC)
-    } else {
-      alert("equilateral");
+function triangle(){
+    var sides = []
+    
+    sides.push(parseInt(prompt("enter first side")));
+    sides.push(parseInt(prompt("enter second side")));
+    sides.push(parseInt(prompt("enter third side")));
+    
+    var first = sides[0];
+    var second = sides[1];
+    var third = sides[2];
+    
+    /* console.log(sides); */
+    if (first===second  &&  second===third && first===third){
+    alert("equilateral")
     }
-  };
-  
-  var triangleType = function(sideA, sideB, sideC){
-    if (sideOne===sideA && sideB===sideC) {
-      alert("equilateral");
-    } else if (sideA===sideB || sideA===sideB || sideC===sideB) {
-      alert("isosceles");
-    } else {
-      alert("scalene");
+    
+    else if (first===second  &&  first!==third || first===third  &&  first!==second || second===third  &&  second!==first){
+    alert("isosceles")
     }
-  };
-  
-  var isTriangle = function(sideA, sideB, sideC) {
-    return sideA <= (sideB + sideC) && sideTwo <= (sideC + sideA) && sideC <= (sideA + sideB)
-  };
-  $(document).ready(function() {
-    $("form#triangle").submit(function(event) {
-  
-      var sideA = parseInt($("input#sideA").val());
-      var sideB = parseInt($("input#sideB").val());
-      var sideC = parseInt($("input#sideC").val());
-      var result = triangle(sideA, sideB, sideC);
-  
-      sideA = parseInt($("input#sideA").val(""));
-      sideB = parseInt($("input#sideB").val(""));
-      sideC = parseInt($("input#sideC").val(""));
-  
-      $("#result").append(result);
-      event.preventDefault();
-    });
-  });
+    
+    else if(first!==second && second!==third && first!==third){
+    alert("scalene")
+    }
+    else {
+    alert("NOT a triangle")
+    }
+    }
